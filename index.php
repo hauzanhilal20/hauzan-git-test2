b317a186a2c2049c47c27c3372387c3d9e22b144
hauzanhilal <hauzanhilal001@gmail.com>
2023-12-01 15:17:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, culpa veritatis. Molestiae maxime inventore dolores culpa magnam obcaecati eveniet unde laudantium explicabo quod fugiat voluptates autem tempore aut, alias repudiandae.</p>
+    <ul>
+        <li>spl_autoload</li>
+        <li>date_sub</li>
+    </ul>
 </body>
 </html>
