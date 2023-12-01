3b4ab02679a4479ec6bfff472b8900ad1d231692
hauzanhilal <hauzanhilal001@gmail.com>
2023-12-01 15:28:28 +0700

    create style.css

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coba git remote</title>
+
 </head>
 <body>
     <h1>hello world</h1>
